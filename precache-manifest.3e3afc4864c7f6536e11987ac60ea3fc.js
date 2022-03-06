d005864fe7d02b4068d9388150767eadc27c2e5e
gfouz <gfouz1975@gmail.com>
2022-03-06 06:44:41 +0100

    Updates

---
+++ precache-manifest.3e3afc4864c7f6536e11987ac60ea3fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ce7645b14867165c819",
+    "url": "/react-website/static/css/main.3d0e8982.chunk.css"
+  },
+  {
+    "revision": "0ce7645b14867165c819",
+    "url": "/react-website/static/js/main.0ce7645b.chunk.js"
+  },
+  {
+    "revision": "9f9cd5acd48533a6d469",
+    "url": "/react-website/static/js/1.9f9cd5ac.chunk.js"
+  },
+  {
+    "revision": "4bd2eb48bc79ce71ea7a",
+    "url": "/react-website/static/js/runtime~main.4bd2eb48.js"
+  },
+  {
+    "revision": "779a92ac45ab7bce8cd4a2189d482ecb",
+    "url": "/react-website/static/media/literata.779a92ac.ttf"
+  },
+  {
+    "revision": "26e7f025d1d409ca6a03102b696b7916",
+    "url": "/react-website/static/media/expanse.26e7f025.otf"
+  },
+  {
+    "revision": "423e7ba576ab0a4498a0a8fe99f0b687",
+    "url": "/react-website/index.html"
+  }
+];
